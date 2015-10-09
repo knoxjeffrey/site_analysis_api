4d6b3fe5386556521322a24585a8ef99b4a1c059
Jeff Knox <knoxjeffrey@outlook.com>
2015-10-09 14:31:57 +0100

    user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,17 +4,24 @@ describe User do
 
   let(:user) { Fabricate.build(:user) }
 
-    subject { user }
+  subject { user }
 
-    it { should be_valid }
-    it { should respond_to(:email) }
-    it { should respond_to(:password) }
-    it { should respond_to(:password_confirmation) }
-    it { should respond_to(:full_name) }
+  it { should be_valid }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:full_name) }
 
-    it { should validate_presence_of(:email) }
-    it { should validate_confirmation_of(:password) }
-    it { should allow_value('foo@domain.com').for(:email) }
-    it { should validate_presence_of(:full_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should validate_presence_of(:full_name) }
 
+  it { should allow_value('foo@domain.com').for(:email) }
+
+  describe "when email is not present" do
+    before { user.email = "" }
+    it { should_not be_valid }
   end
+
+
+end
