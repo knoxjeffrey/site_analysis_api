cbbbb9b2cede6063cff07f54c1c4a2dea74b1f2d
Jeff Knox <knoxjeffrey@outlook.com>
2015-10-09 16:33:00 +0100

    project controller create  action tests

--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -4,6 +4,45 @@ describe Api::V1::ProjectsController do
 
   before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
 
+  describe "POST create" do
+
+    let!(:user) { Fabricate(:user) }
+
+    context "with a valid access token" do
+
+      let(:token) { double :acceptable? => true, :scopes => [:api],
+                    :resource_owner_id => user.id }
+
+      before do
+        allow(controller).to receive(:doorkeeper_token) {token}
+        post :create, project: Fabricate.attributes_for(:project), format: :json
+      end
+
+      it "creates a new project" do
+        expect(Project.count).to eq(1)
+      end
+
+      it "creates a new project associated with the user" do
+        project_response = JSON.parse(response.body, symbolize_names: true)
+        expect(project_response[:admin_id]).to eq user.id
+      end
+
+    end
+
+    context "with an invalid access token" do
+
+      let(:token) { double :acceptable? => false, :scopes => [:api],
+                    :resource_owner_id => user.id }
+
+      it "does not create a new project" do
+        post :create, project: Fabricate.attributes_for(:project), format: :json
+        expect(Project.count).to eq(0)
+      end
+
+    end
+
+  end
+
   describe "GET show" do
 
     let!(:user) { Fabricate(:user) }
@@ -21,6 +60,11 @@ describe Api::V1::ProjectsController do
         get :show, id: project.id, format: :json
         project_response = JSON.parse(response.body, symbolize_names: true)
         expect(project_response[:project_name]).to eql "Test Project"
+      end
+
+      it "returns a 200 response code" do
+        get :show, id: project.id, format: :json
+        expect(response.code).to eq("200")
       end
 
     end
